cfcb966003f1a5f6b4a9f45ea18c8f1bd56378e1
pedrowagnerbomfim <pedrowagnerbomfim@gmail.com>
2020-08-02 12:41:02 -0300

    feat: add search term and prefix

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+const readline = require('readline-sync')
+
 function start(){
     const content = {}
 
     content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
 
     function askAndReturnSearchTerm(){
-        return 'Term Exemple'
+        return readline.question('Type a Wikipidia search term: ')
+    }
+
+    function askAndReturnPrefix (){
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose a option:')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        console.log(selectedPrefixText)
+
+        return selectedPrefixText
     }
     console.log(content)
 }
