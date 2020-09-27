ace7082dca983c53eb108059dc199ea12b43b561
pedrowagnerbomfim <pedrowagnerbomfim@gmail.com>
2020-09-27 17:14:38 -0300

    feat: add input robot and state robot

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-const readline = require("readline-sync");
 const robots = {
-  text: require("./robots/text.js"),
-};
-
-async function start() {
-  const content = {
-    maximumSentences: 10
-  }
-
-  content.searchTerm = askAndreturnSearchTerm();
-  content.prefix = askAndReturnPrefix();
-
-  await robots.text(content);
-
-  function askAndreturnSearchTerm() {
-    return readline.question("Type a Wikipedia search term:");
-  }
-
-  function askAndReturnPrefix() {
-    const prefixes = ["Who is", "What is", "The history of"];
-    const selectedPrefixIndex = readline.keyInSelect(
-      prefixes,
-      "Choose a option:"
-    );
-    const selectedPrefixText = prefixes[selectedPrefixIndex];
-
-    return selectedPrefixText;
-  }
+  input: require('./robots/input.js'),
+  text: require('./robots/text.js'),
+  state: require('./robots/state.js'),
 }
 
-start();
+async function start() {
+  robots.input()
+  await robots.text()
+}
+
+start()
