bef72d4a46fd302e4b3730b6cf4b71b3a92b52b2
pedrowagnerbomfim <pedrowagnerbomfim@gmail.com>
2020-12-01 22:20:20 -0300

    feat: add input robot and state robot

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const robots = {
   input: require('./robots/input.js'),
   text: require('./robots/text.js'),
-  state: require('./robots/state.js'),
+  state: require('./robots/state.js')
 }
 
 async function start() {
